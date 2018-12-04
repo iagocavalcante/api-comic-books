1b288bd948cba32a46ce3990612dd7cfbdd71587
Iago Cavalcante <iagoangelimc@gmail.com>
2018-12-04 16:49:40 -0300

    Manage comic book

--- src/repositories/comic-book-repository.js
+++ src/repositories/comic-book-repository.js
@@ -1,64 +1,72 @@
-const mongoose = require('mongoose')
-const ComicBook = mongoose.model('ComicBook')
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogActions from '@material-ui/core/DialogActions';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import withRoot from '../withRoot';
 
-exports.get = async () => {
-  const res = await ComicBook.aggregate([
-    {
-      $lookup: {
-        from: "publisher",
-        localField: "publisher_id",
-        foreignField: "_id",
-        as: "publisher"
-      }
-    }
-  ])
-  return res
+const styles = theme => ({
+  root: {
+    textAlign: 'center',
+    paddingTop: theme.spacing.unit * 20,
+  },
+});
+
+class Index extends React.Component {
+  state = {
+    open: false,
+  };
+
+  handleClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
+  handleClick = () => {
+    this.setState({
+      open: true,
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { open } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <Dialog open={open} onClose={this.handleClose}>
+          <DialogTitle>Super Secret Password</DialogTitle>
+          <DialogContent>
+            <DialogContentText>1-2-3-4-5</DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button color="primary" onClick={this.handleClose}>
+              OK
+            </Button>
+          </DialogActions>
+        </Dialog>
+        <Typography variant="h4" gutterBottom>
+          Material-UI
+        </Typography>
+        <Typography variant="subtitle1" gutterBottom>
+          example project
+        </Typography>
+        <Button variant="contained" color="secondary" onClick={this.handleClick}>
+          Super Secret Password
+        </Button>
+      </div>
+    );
+  }
 }
 
-exports.getBySlug = async (slug) => {
-  const res = await ComicBook
-    .findOne({
-      slug: slug
-    }, 'title description pages slug read')
-  return res
-}
+Index.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
-exports.getById = async (id) => {
-  const res = await ComicBook
-    .findById(id)
-  return res
-}
-
-exports.getBySaga = async (saga) => {
-  const res = ComicBook
-    .find({
-      saga: saga
-    }, 'title description pages slug saga published_date published_id read')
-  return res
-}
-
-exports.create = async (data) => {
-  await ComicBook.create(data)
-}
-
-exports.update = async (id, data) => {
-  await ComicBook
-    .findByIdAndUpdate(id, {
-      $set: {
-        title: data.title,
-        description: data.description,
-        pages: data.pages,
-        slug: data.slug,
-        saga: data.saga,
-        read: data.read,
-        image: data.image,
-        publisher_id: data.publisher_id,
-        published_date: data.published_date,
-      }
-    })
-}
-
-exports.delete = async (id) => {
-  await ComicBook
-    .findOneAndRemove(id)
-}
+export default withRoot(withStyles(styles)(Index));
